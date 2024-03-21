7a4612e0e6e770f3f573d235588e11ebd5d67d79
Uditraj Rout <103300757+Uditraj07@users.noreply.github.com>
2024-03-21 23:12:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const http = require('http');
-const fs=require('fs');
-const { resolve } = require('path');
-const route=require('./route')
-const server = http.createServer(route);
-server.listen(3000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const express=require('express');
+const http=require('http');
+const app=express();
+const bodyParse=require('body-parser');
+app.use(bodyParse.urlencoded({extended:false}))
+app.use(bodyParse.json());
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text"name="title" placeholder="enterName"><input type="text" name="size" placeholder="enter size"><button type="submit">Submit</button</form>')
+})
+app.use('/product',(req,res,next)=>{
+    console.log(req.body.title)
+    console.log(req.body.size)
+    res.redirect('/')
+})
+app.use('/get-postman',(req,res,next)=>{
+    console.log(req.body);
+    res.send('Received successfully')
+})
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hellow From / path</h1>')
+})
+app.listen(3000);
